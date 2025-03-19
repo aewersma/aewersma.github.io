f379ab93d8f1b1e2c4384226912028fcb640eb02
aewersma <aewersma@charlotte.edu>
2025-03-19 11:16:29 -0400

    19/03/2025 - 3

--- hobby/scripts/script.js
+++ hobby/scripts/script.js
@@ -3,12 +3,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const navLinks = document.querySelectorAll("nav ul li a");
 
     function showSection(targetId) {
-        sections.forEach(section => {
+        sections.forEach((section) => {
             section.style.display = section.id === targetId ? "block" : "none";
         });
     }
 
-    navLinks.forEach(link => {
+    navLinks.forEach((link) => {
         link.addEventListener("click", function (event) {
             event.preventDefault();
             const targetId = this.getAttribute("href").substring(1);
